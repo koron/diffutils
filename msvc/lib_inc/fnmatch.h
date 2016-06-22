e8274e19bdf2904422158ad83067339d016b4a45
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-06-23 00:33:17 +0900

    fnmatch dummy

---
+++ msvc/lib_inc/fnmatch.h
@@ -0,0 +1,6 @@
+#ifndef	_FNMATCH_H_
+#define	_FNMATCH_H_
+
+int	 fnmatch(const char *, const char *, int);
+
+#endif /* !_FNMATCH_H_ */
---
+++ msvc/lib_src/fnmatch.c
@@ -0,0 +1,8 @@
+#include <fnmatch.h>
+
+int
+fnmatch(const char *spec, const char *fname, int flag)
+{
+    /* TODO: PathMatchSpecEx: https://msdn.microsoft.com/en-us/library/bb773728(v=vs.85).aspx*/
+    return 0;
+}
